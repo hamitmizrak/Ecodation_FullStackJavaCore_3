4ec8d983e16742bbc2296ddd98c09b8d44171234
Hamit Mızrak <hamitmizrak@gmail.com>
2025-12-06 12:20:34 +0300

    Single comment, multiple comment psvm, sout

--- src/main/java/com/hamitmizrak/_1_WhatIsJava.java
+++ src/main/java/com/hamitmizrak/_1_WhatIsJava.java
@@ -3,7 +3,13 @@ package com.hamitmizrak;
 // Single comment
 /*
 Multiple Comment
-
  */
 public class _1_WhatIsJava {
+
+    /*psvm => TAB*/
+    static void main() {
+        // sout => TAB
+        System.out.println("Merhabalar");
+
+    }
 }
