f164c786d2733f27256bb0b380bceb3517ee91e3
Hamit Mızrak <hamitmizrak@gmail.com>
2025-12-07 11:22:04 +0300

    Scanner

--- src/main/java/com/hamitmizrak/_6_Scanner.java
+++ src/main/java/com/hamitmizrak/_6_Scanner.java
@@ -1,4 +1,21 @@
 package com.hamitmizrak;
 
+import java.util.Scanner;
+
 public class _6_Scanner {
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        String name,surname;
+
+        // Escape Character  \"
+        System.out.print("\nLütfen adınız giriniz: ");
+        //name= scanner.nextLine();
+        name= scanner.nextLine();
+
+        System.out.print("Lütfen soyadınız giriniz: ");
+        //surname= scanner.nextLine();
+        surname= scanner.nextLine();
+
+        System.out.println("Adınız ve Soyadınız: ==>  "+ (name+" "+surname));
+    }
 }
