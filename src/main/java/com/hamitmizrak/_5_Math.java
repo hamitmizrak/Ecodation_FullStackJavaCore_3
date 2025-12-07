ec7ef1aa2faeb6f157b40766e59244f769049013
Hamit Mızrak <hamitmizrak@gmail.com>
2025-12-07 10:36:01 +0300

    Math

--- src/main/java/com/hamitmizrak/_5_Math.java
+++ src/main/java/com/hamitmizrak/_5_Math.java
@@ -1,4 +1,20 @@
 package com.hamitmizrak;
 
 public class _5_Math {
+
+    static void main() {
+        System.out.println("En küçük sayı: "+Math.min(10,900));
+        System.out.println("En büyük sayı: "+Math.max(10,900));
+
+        System.out.println("Karekök: "+Math.sqrt(16));
+        System.out.println("Mutlak: "+Math.abs(-16));
+        System.out.println("Üslü: "+Math.pow(2,5));
+
+        System.out.println("Sinüs: "+Math.sin(45));
+        System.out.println("Cosinüs: "+Math.cos(45));
+
+        System.out.println("alta yuvarla: "+Math.floor(14.9));
+        System.out.println("üste yuvarla: "+Math.ceil(14.0001));
+        System.out.println("ortalama yuvarla: "+Math.round(14.1));
+    }
 }
