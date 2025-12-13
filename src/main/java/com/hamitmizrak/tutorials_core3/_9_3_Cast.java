caf42890cda46371ad7a17a9087d9764a80d8a3d
Hamit Mızrak <hamitmizrak@gmail.com>
2025-12-13 09:55:52 +0300

    String

--- src/main/java/com/hamitmizrak/tutorials_core3/_9_3_Cast.java
+++ src/main/java/com/hamitmizrak/tutorials_core3/_9_3_Cast.java
@@ -4,5 +4,17 @@ public class _9_3_Cast {
 
     static void main() {
 
+        // String ==> Integer dönüştür
+        String data1="4";
+        int data2=Integer.valueOf(data1);
+        int data3=16;
+
+        System.out.println(data1+data3);
+        System.out.println(data2+data3);
+
+        // Integer ==> String
+        int data4=4;
+        String data5= String.valueOf(data4);
+        System.out.println(data5+data3);
     }
 }
